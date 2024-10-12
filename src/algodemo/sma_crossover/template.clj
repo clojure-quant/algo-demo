9013abcc892770225584c035b76fe55e96a252e9
awb99 <awb99@gmail.com>
2024-10-11 19:57:28 -0500

    algo adjusted to new format

--- src/algodemo/sma_crossover/template.clj
+++ src/algodemo/sma_crossover/template.clj
@@ -1,91 +1,119 @@
-(ns algodemo.sma-crossover.template)
+(ns algodemo.sma-crossover.template
+  (:require
+   [quanta.viz.plot :as plot]
+   [quanta.trade.backtest :refer [backtest]]
+   [quanta.viz.plot.trade.core :refer [roundtrip-stats-ui]]
+   [algodemo.sma-crossover.algo :refer [sma-crossover-algo]]))
 
-
-(def sma-crypto
-  {:id :sma-crypto
-   :algo {:type :trailing-bar
-          :calendar [:forex :m]
-          :trailing-n 1000
-          :asset "ETHUSDT"
-          :feed :bybit
-          :import :bybit
-          :algo 'algodemo.sma-crossover.algo/sma-crossover-algo
+(def crossover-algo
+  [{:asset "BTCUSDT"}
+   :algo {:algo sma-crossover-algo
+          :calendar [:crypto :h]
+          :bardb :nippy
+          :trailing-n 1440
+          ; algo specific parameters
           :sma-length-st 20
           :sma-length-lt 200}
-   :options [{:type :select
-              :path :asset
-              :name "Asset"
-              :spec ["BTCUSDT" "ETHUSDT"]}
-             {:type :select
-              :path :trailing-n
-              :name "trailing-n"
-              :spec [100 300 500 1000 2000 3000 5000 10000]}
-             {:type :select
-              :path :sma-length-st
-              :name "sma-st"
-              :spec [10 20 50 100]}
-             {:type :select
-              :path :sma-length-lt
-              :name "sma-lt"
-              :spec [100 200 500 1000]}]
-   :chart {:viz 'ta.viz.ds.highchart/highstock-render-spec
-           :viz-options {:chart {:box :fl}
-                         :charts [{:bar :candlestick ; :ohlc ; :line 
-                                   :close :line
-                                   :sma-lt :line
-                                   :sma-st :line
-                                   ;:signal :flags
-                                   :signal {:type :flags 
-                                            :fillColor "blue"
-                                            :width 10
-                                            :height 10
-                                            :v2style {;:long "square"
-                                                      :long "url(/r/arrow-up.svg)"
-                                                      true "flags"
-                                                      ;:short "circle"
-                                                      :short "url(/r/arrow-down.svg)"
-                                                       }}}
-                                  #_{:volume :column}]}}
-   :table {:viz 'ta.viz.ds.rtable/rtable-render-spec
-           :viz-options {:columns [{:path :date}
-                                   {:path :close}
-                                   {:path :sma-st}
-                                   {:path :sma-lt}
-                                   {:path :position}
-                                   {:path :signal}]}}
-   :metrics {:viz 'ta.viz.ds.metrics/metrics-render-spec
-             :viz-options {}}})
+   :backtest {:formula [:algo]
+              :algo backtest
+              ; position management
+              :entry [:fixed-amount 100000]
+              :exit [:loss-percent 2.0
+                     :profit-percent 1.0
+                     :time 5]}])
+
+(def crossover-options
+  [{:type :select
+    :path [0 :asset]
+    :name "Asset"
+    :spec ["BTCUSDT" "ETHUSDT"]}
+   {:type :select
+    :path [2 :trailing-n]
+    :name "trailing-n"
+    :spec [100 300 500 1000 2000 3000 5000 10000]}
+   {:type :select
+    :path [2 :sma-length-st]
+    :name "sma-st"
+    :spec [10 20 50 100]}
+   {:type :select
+    :path [2 :sma-length-lt]
+    :name "sma-lt"
+    :spec [100 200 500 1000]}])
 
 
+(def sma-chart
+  {:viz 'ta.viz.ds.highchart/highstock-render-spec
+   :key :algo
+   :viz-options {:chart {:box :fl}
+                 :charts [{:bar :candlestick ; :ohlc ; :line 
+                           :close :line
+                           :sma-lt :line
+                           :sma-st :line
+                                     ;:signal :flags
+                           :signal {:type :flags
+                                    :fillColor "blue"
+                                    :width 10
+                                    :height 10
+                                    :v2style {;:long "square"
+                                              :long "url(/r/arrow-up.svg)"
+                                              true "flags"
+                                                        ;:short "circle"
+                                              :short "url(/r/arrow-down.svg)"}}}
+                          #_{:volume :column}]}})
 
-(def eodhd-eod
-  {:id :sma-eodhd-eod
-   :algo {:type :trailing-bar
-          :algo 'algodemo.sma-crossover.algo/sma-crossover-algo
-          :calendar [:us :d]
-          :asset ""
-          :import :eodhd
-          :trailing-n 400
-          :sma-length-st 20
-          :sma-length-lt 200}
-   :options [{:type :asset-picker ; custom DYNAMIC UI!!!
-              :path :asset
-              :name "Asset"}
-             {:type :select
-              :path :trailing-n
-              :name "trailing-n"
-              :spec [200 400 600 800 1000 2000 3000 5000 10000]}
-             {:type :select
-              :path :sma-length-st
-              :name "sma-st"
-              :spec [10 20 50 100]}
-             {:type :select
-              :path :sma-length-lt
-              :name "sma-lt"
-              :spec [100 200 500 1000]}]
-   :chart {:viz 'ta.viz.ds.highchart/highstock-render-spec
-           :viz-options {:chart {:box :fl}
-                         :charts [{:bar :candlestick ; :ohlc ; :line 
-                                   :sma-lt :line
-                                   :sma-st :line}
-                                  #_{:volume :column}]}}})
+(def sma-table
+  {:viz 'ta.viz.ds.rtable/rtable-render-spec
+   :key :algo
+   :viz-options {:columns [{:path :date}
+                           {:path :close}
+                           {:path :sma-st}
+                           {:path :sma-lt}
+                           {:path :position}
+                           {:path :signal}]}})
+
+(def sma-backtest 
+  {:key :backtest
+   :viz roundtrip-stats-ui
+   :viz-options {}}
+  )
+
+(def crossover-template
+  {:id :sma-crossover
+   :algo crossover-algo
+   :options crossover-options
+   :chart sma-chart
+   :table sma-table
+   :backtest sma-backtest})
+
+
+#_(def eodhd-eod
+    {:id :sma-eodhd-eod
+     :algo {:type :trailing-bar
+            :algo 'algodemo.sma-crossover.algo/sma-crossover-algo
+            :calendar [:us :d]
+            :asset ""
+            :import :eodhd
+            :trailing-n 400
+            :sma-length-st 20
+            :sma-length-lt 200}
+     :options [{:type :asset-picker ; custom DYNAMIC UI!!!
+                :path :asset
+                :name "Asset"}
+               {:type :select
+                :path :trailing-n
+                :name "trailing-n"
+                :spec [200 400 600 800 1000 2000 3000 5000 10000]}
+               {:type :select
+                :path :sma-length-st
+                :name "sma-st"
+                :spec [10 20 50 100]}
+               {:type :select
+                :path :sma-length-lt
+                :name "sma-lt"
+                :spec [100 200 500 1000]}]
+     :chart {:viz 'ta.viz.ds.highchart/highstock-render-spec
+             :viz-options {:chart {:box :fl}
+                           :charts [{:bar :candlestick ; :ohlc ; :line 
+                                     :sma-lt :line
+                                     :sma-st :line}
+                                    #_{:volume :column}]}}})
